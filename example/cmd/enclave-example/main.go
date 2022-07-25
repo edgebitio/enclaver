e5c3185c13be9b6790c7a1120b5b9f5df83973ad
Russell Haering <russellhaering@gmail.com>
2022-07-24 22:57:25 -0700

    Modify the example app to proxy HTTP traffic over the vsock

--- example/cmd/enclave-example/main.go
+++ example/cmd/enclave-example/main.go
@@ -1,15 +1,77 @@
 package main
 
 import (
+	"fmt"
 	"github.com/mdlayher/vsock"
 	"golang.org/x/sys/unix"
+	"io"
+	"net"
+	"net/http"
+	"os"
 )
 
 func main() {
-	err := listen()
+	os.Setenv("HTTP_PROXY", "http://localhost:3128")
+	os.Setenv("HTTPS_PROXY", "http://localhost:3128")
+	runInternalProxy()
+
+	resp, err := http.Get("https://google.com")
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Printf("Got status: %d\n", resp.StatusCode)
+
+	/*
+		err = listen()
+		if err != nil {
+			panic(err)
+		}
+	*/
+}
+
+// TODO: any error handling or logging at all
+func runInternalProxy() error {
+	listener, err := net.Listen("tcp", ":3128")
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		defer listener.Close()
+
+		for {
+			clientConn, err := listener.Accept()
+			if err != nil {
+				panic(err)
+			}
+
+			println("accepted proxy conn...")
+
+			go func() {
+				serverConn, err := vsock.Dial(3, 3128, nil)
+				if err != nil {
+					panic(err)
+				}
+
+				errc := make(chan error)
+
+				go func() {
+					_, err := io.Copy(serverConn, clientConn)
+					errc <- err
+				}()
+
+				go func() {
+					_, err := io.Copy(clientConn, serverConn)
+					errc <- err
+				}()
+
+				<-errc
+			}()
+		}
+	}()
+
+	return nil
 }
 
 func listen() error {
