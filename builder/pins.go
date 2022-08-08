2dbd172f676a12d56c7842caf8db2f0d83debe00
Russell Haering <russellhaering@gmail.com>
2022-08-08 14:17:43 -0700

    Update pins

--- builder/pins.go
+++ builder/pins.go
@@ -2,5 +2,5 @@ package builder
 
 const (
 	nitroCLIContainerDigest       = "sha256:7fd9a8f06dbccb5ae217aa9711d44dd28887cca6e81d2f513ec66d038a0aafad"
-	enclaveWrapperContainerDigest = "sha256:0b95f5d14d4ce9828037f2605ce1b0da43959254f19213b8b34cb1a67468ddb3"
+	enclaveWrapperContainerDigest = "sha256:560a1a509e06838b1d682c6bcb89cdf13342aeab727725fa638d332e45fb10df"
 )
