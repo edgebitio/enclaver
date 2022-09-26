c9f4021f9f516454217b62fb9d432a7ce69b0977
Russell Haering <russell@edgebit.io>
2022-09-26 16:51:01 -0700

    Rip out all of the Go code

--- builder/pins.go
+++
@@ -1,6 +0,0 @@
-package builder
-
-const (
-	nitroCLIContainerDigest       = "sha256:7fd9a8f06dbccb5ae217aa9711d44dd28887cca6e81d2f513ec66d038a0aafad"
-	enclaveWrapperContainerDigest = "sha256:adba7aca2ae0d3ba86c2f3112a732d4fd7971215c5c592aaf6de10eb0434dde7"
-)
--- vendor/github.com/hf/nsm/.vimrc
+++
@@ -1,4 +0,0 @@
-let NERDTreeIgnore = ['.git$', '.cache$', 'node_modules$']
-let NERDTreeShowHidden = 1
-
-:set backupcopy=yes
