624b938e8672847e750498c597a43288d5e0f7a3
Russell Haering <russell@edgebit.io>
2022-09-28 11:51:52 -0700

    Fix zig target

--- .github/actions/cargo-zig/entrypoint.sh
+++ .github/actions/cargo-zig/entrypoint.sh
@@ -4,6 +4,19 @@ TARGET="$1"
 
 shift
 
+case $TARGET in
+  "aarch64-unknown-linux-musl")
+    ZIG_TARGET="aarch64-linux-musl"
+    ;;
+  "x86_64-unknown-linux-musl")
+    ZIG_TARGET="x86_64-linux-musl"
+    ;;
+  *)
+    echo "Unsupported architecture: ${TARGET}"
+    exit 1
+    ;;
+esac
+
 ZIG_COMMAND="zig cc -target ${ZIG_TARGET}"
 
 export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER="${ZIG_COMMAND}"
