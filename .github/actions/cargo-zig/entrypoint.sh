0a1f7a80d1746ee3053dc22f58e8862dac92b2a4
Russell Haering <russell@edgebit.io>
2022-09-28 09:45:19 -0700

    Experiment with zig builder action

---
+++ .github/actions/cargo-zig/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TARGET="$1"
+
+shift
+
+ZIG_COMMAND="zig cc -target ${ZIG_TARGET}"
+
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER="${ZIG_COMMAND}"
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER="${ZIG_COMMAND}"
+export CC="${ZIG_COMMAND}"
+
+cargo "$@" "--target=${TARGET}"
