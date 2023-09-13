edgebitio/enclaver

commit 53875afcf6d503d6e6dd5a1ecbdc963990ea3888

Address the remaining Clippy warnings

enclaver/src/proxy/kms.rs
@@ -19,7 +19,7 @@ use crate::http_util::HttpHandler;
 use crate::keypair::KeyPair;
 use crate::nsm::{AttestationParams, AttestationProvider};
 
-const X_AMZ_TARGET: HeaderName = HeaderName::from_static("x-amz-target");
+static X_AMZ_TARGET: HeaderName = HeaderName::from_static("x-amz-target");
 
 static X_AMZ_JSON: HeaderValue = HeaderValue::from_static("application/x-amz-json-1.1");
 
@@ -107,7 +107,7 @@ impl KmsRequestIncoming {
     }
 
     fn target(&self) -> Option<&HeaderValue> {
-        self.head.headers.get(X_AMZ_TARGET)
+        self.head.headers.get(&X_AMZ_TARGET)
     }
 
     fn content_type(&self) -> &HeaderValue {
@@ -131,7 +131,7 @@ impl KmsRequestIncoming {
         false
     }
 
-    fn credential_scope<'a>(&'a self) -> Result<CredentialScope> {
+    fn credential_scope(&self) -> Result<CredentialScope> {
         CredentialScope::from_request(&self.head)
     }
 }
@@ -153,7 +153,7 @@ impl KmsRequestOutgoing {
         let inner = Request::builder()
             .method(Method::POST)
             .uri(uri)
-            .header(X_AMZ_TARGET, action)
+            .header(&X_AMZ_TARGET, action)
             .header(hyper::header::CONTENT_TYPE, &X_AMZ_JSON)
             .body(body_bytes)?;
 
@@ -172,7 +172,7 @@ impl KmsRequestOutgoing {
         let inner = Request::builder()
             .method(req_in.method())
             .uri(uri)
-            .header(X_AMZ_TARGET, action)
+            .header(&X_AMZ_TARGET, action)
             .header(hyper::header::CONTENT_TYPE, req_in.content_type())
             .body(req_in.body)?;
 

enclaver/Cargo.toml
@@ -68,6 +68,7 @@ asn1-rs = "0.5.2"
 cbc = { version = "0.1", features = [ "std", "block-padding" ] }
 aes = "0.8"
 sha2 = "0.10"
+ignore-result = "0.2.0"
 
 
 [dev-dependencies]

enclaver/src/bin/odyn/console.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use circbuf::CircBuf;
 use futures::Stream;
+use ignore_result::Ignore;
 use std::os::unix::io::AsRawFd;
 use std::sync::{Arc, Mutex};
 use tokio::io::{AsyncReadExt, AsyncWrite, AsyncWriteExt};
@@ -47,7 +48,7 @@ impl ByteLog {
         let avail = self.buffer.avail();
         if avail < data.len() {
             trim_cnt = data.len() - avail;
-            self.buffer.advance_read(trim_cnt);
+            self.buffer.advance_read(trim_cnt).ignore();
             self.head += trim_cnt;
         }
         assert!(self.buffer.avail() >= data.len());

enclaver/src/nitro_cli.rs
@@ -4,7 +4,7 @@ use anyhow::{anyhow, Result};
 use log::debug;
 use serde::{Deserialize, Serialize};
 use std::ffi::OsString;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Stdio;
 use tokio::process::{ChildStdout, Command};
 
@@ -68,7 +68,7 @@ impl NitroCLI {
         }
     }
 
-    pub async fn describe_eif(&self, eif_path: &PathBuf) -> Result<EIFInfo> {
+    pub async fn describe_eif(&self, eif_path: &Path) -> Result<EIFInfo> {
         self.run_and_deserialize_output(DescribeEifArgs {
             eif_path: eif_path.to_path_buf(),
         })

enclaver/src/proxy/ingress.rs
@@ -90,7 +90,7 @@ impl HostProxy {
         while let Ok((sock, _)) = self.listener.accept().await {
             // TODO: don't use detached tasks
             tokio::task::spawn(async move {
-                HostProxy::service_conn(sock, target_cid, target_port).await;;
+                HostProxy::service_conn(sock, target_cid, target_port).await;
             });
         }
     }

commit 0a47151652676cd0613648cc0f349484869f519e

Get tests compiling again
This was broken way back in 53875af. I hadn't noticed since I wasn't
running the tests locally and our CI doesn't run them.

enclaver/src/proxy/kms.rs
@@ -452,7 +452,7 @@ mod tests {
             &self,
             req: Request<Body>,
         ) -> std::result::Result<Response<Body>, hyper::Error> {
-            let action = req.headers().get(X_AMZ_TARGET).unwrap().to_str().unwrap();
+            let action = req.headers().get(&X_AMZ_TARGET).unwrap().to_str().unwrap();
 
             let authz = req
                 .headers()
@@ -513,7 +513,7 @@ mod tests {
         Request::builder()
             .method(Method::POST)
             .uri("/")
-            .header(X_AMZ_TARGET, action)
+            .header(&X_AMZ_TARGET, action)
             .header(hyper::header::CONTENT_TYPE, &X_AMZ_JSON)
             .header(
                 hyper::header::AUTHORIZATION,